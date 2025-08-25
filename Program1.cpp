041cd9d368266bf56d16ee940838f7af0bd765c5
kulkarnisharvari1709 <kulkarnisharvari1709@gmail.com>
2025-08-25 15:03:27 +0530

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,30 @@
+// Example: defining the constructor inside the class
+#include<iostream>
+using namespace std;
+class student
+{
+    int rollno;
+    char name[50];
+    double fee;
+    public:
+student()
+    {
+        cout<<"Enter the RollNo:";
+        cin>> rollno;
+        cout<<"Enter the Name:";
+        cin>>name;
+        cout<<"Enter the Fee:";           cin>>fee;      }   
+
+void display()
+{
+        cout<<endl<< rollno <<"\t"<<name<<"\t"<<fee;
+    }
+};
+
+int main()
+{
+    student s;        //constructor gets called automatically when we create the    
+                                  object of the class
+s.display();
+    return 0;
+ }
