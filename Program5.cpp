692a169a160ceec514b1a0aa644d84081328ad84
kulkarnisharvari1709 <kulkarnisharvari1709@gmail.com>
2025-08-25 15:34:04 +0530

    Create Program5.cpp

---
+++ Program5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+usingnamespacestd;
+
+//Defining parametrized constructor
+classNum{
+    //int a=7, b=9;
+    public:
+    Num(intc, intd){
+        if(c>d){
+            cout<<c<<" is greater";
+        }
+        else{
+            cout<<d<<" is greater";
+        }
+    }
+};
+
+intmain(){
+    Numn1(4,3);
+}
